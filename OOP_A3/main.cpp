54eb09bb3b767bd0ce690eeadb89efeb85408108
n3030503 <n3030503@x2go>
2026-03-25 11:17:36 +0100

    X2GO Version

--- OOP_A3/main.cpp
+++ OOP_A3/main.cpp
@@ -19,5 +19,7 @@ int ggt_2 = ggt2(45, 100);
 cout << "Größter gemeinsamer Teiler von " << x << " & " << y << ": " << ggt << endl;
 cout << "Größter gemeinsamer Teiler von " << x << " & " << y << ": " << ggt_2 << endl;
 
+//X2GO Version
+
 return 0;
 }
