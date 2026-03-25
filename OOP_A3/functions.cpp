7be1b0b3f4846b20db8f239a0cca6878573bbe73
swt <swt@pb25>
2026-03-25 18:48:00 +0100

    OOP_A3 fertig

--- OOP_A3/functions.cpp
+++ OOP_A3/functions.cpp
@@ -1,42 +1,32 @@
-#include <iostream>
 #include "header.h"
 using namespace std;
 
 int ggt1(int x, int y){
-	if (x < y){
-	y = y - x;
-
-}
-
-	if (y < x){
-	x = x - y;
-
-}
 
 	if (y == x){
-	return x;
-}
+		return x;
+	}
+	if (x > y) {
+	        return ggt1(x - y, y); 
+	    } else {
+	        return ggt1(x, y - x);
+	    }
 
-return ggt1(x, y);
 }
 
 int ggt2(int x, int y){
-	
-	if (x < y){
-	y = y % x;
-}
+	if (y == 0) {
+        return x;
+    }
 
-	if (y < x){
-	x = x % y;
-}
+    if (x == 0) {
+        return y;
+    }
 
-	if (x == 0){
-	return y;
-}
 
-	if (y == 0){
-	return x;
-}
-
-return ggt2(x, y);
+    if (x > y) {
+        return ggt2(x % y, y);
+    } else {
+        return ggt2(x, y % x);
+    }
 }
--- OOP_A3/main.cpp
+++ OOP_A3/main.cpp
@@ -5,19 +5,41 @@
 using namespace std;
 
 int main(int argc, const char* argv[]){
-int x = 45;
-int y = 10;
-int ggt = ggt1(45, 10);
-int ggt_2 = ggt2(45, 100);
 
 	args_t args(argc, argv);
 	string fname = args.pos(0, "ggts.dat"); // (position, default-Wert)
 	Timer timer;
 	vector<int> zahlen = read_ints(fname);
-	cout << "lesen dauerte " << timer.human_measure() << endl;
+	vector<int> ggts1;
+	vector<int> ggts2;
+	int summe_ggt1 = 0, summe_ggt2 = 0;
+	cout << "Einlesen dauerte " << timer.human_measure() << endl;
 
-cout << "Größter gemeinsamer Teiler von " << x << " & " << y << ": " << ggt << endl;
-cout << "Größter gemeinsamer Teiler von " << x << " & " << y << ": " << ggt_2 << endl;
+	Timer t_ggt1;
+	for(int i = 0; i < zahlen.size(); i = i + 2){
+		ggts1.push_back(ggt1(zahlen[i], zahlen[i+1]));
+	}
+	cout << "Berechnung mit ggt1 dauerte " << t_ggt1.human_measure() << endl;
+
+
+	Timer t_ggt2;
+	for(int i = 0; i < zahlen.size(); i = i + 2){
+		ggts2.push_back(ggt2(zahlen[i], zahlen[i+1]));
+	}
+	cout << "Berechnung mit ggt2 dauerte " << t_ggt2.human_measure() << endl;
+
+
+	for(int i = 0; i < ggts1.size(); i++){
+		summe_ggt1 = summe_ggt1 + ggts1[i];
+	}
+
+
+	for(int i = 0; i < ggts2.size(); i++){
+		summe_ggt2 = summe_ggt2 + ggts2[i];
+	}
+
+	cout << "Die Summe aller ggt1 lautet " << summe_ggt1 << endl;
+	cout << "Die Summe aller ggt2 lautet " << summe_ggt2 << endl;
 
 return 0;
 }
